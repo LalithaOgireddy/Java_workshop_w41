65e8dd0a6308aeb6b9e89f609345bede79f0b361
Lalitha Ogireddy <lalitha.ogireddy@gmail.com>
2024-10-09 16:15:17 +0200

    output print changes

--- src/main/java/com/lexicon/Main.java
+++ src/main/java/com/lexicon/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input1 = new Scanner(System.in);
         boolean nextOp = true;
         while(nextOp) {
             System.out.println("\nOperations: ");
@@ -16,32 +16,42 @@ public class Main {
             System.out.println("4. Division");
             System.out.println("5. Exit");
             System.out.println("Enter choice of operation: ");
-            int op = input.nextInt();
+            int op = input1.nextInt();
+            double result = 0;
+            int inputNum;
             if(op != 5) {
-                System.out.println("Enter number1: ");
-                int num1 = input.nextInt();
-                System.out.println("Enter number2: ");
-                int num2 = input.nextInt();
-                int result;
-                switch (op) {
-                    case 1:
-                        result = num1 + num2;
-                        System.out.printf("Result: %d + %d = %d", num1, num2, result);
-                        break;
-                    case 2:
-                        result = num1 - num2;
-                        System.out.printf("Result: %d - %d = %d", num1, num2, result);
-                        break;
-                    case 3:
-                        result = num1 * num2;
-                        System.out.printf("Result: %d * %d = %d", num1, num2, result);
-                        break;
-                    case 4:
-                        result = num1 / num2;
-                        System.out.printf("Result: %d / %d = %d", num1, num2, result);
-                        break;
-                    default:
-                        System.out.println("Enter proper input.");
+                if(op==1 || op==2){
+                    System.out.println("How many operands you want to give: ");
+                    int inputCnt = input1.nextInt();
+                    System.out.println("Enter input numbers separated by space: ");
+                    for(int i=0;i<inputCnt;i++){
+                        if(op==1) {
+                            inputNum = input1.nextInt();
+                            result += inputNum;
+                        } else {
+                            inputNum = input1.nextInt();
+                            result -= inputNum;
+                        }
+                    }
+
+                    System.out.printf("Result: %f",result);
+                } else {
+                    System.out.println("Enter number1: ");
+                    int num1 = input1.nextInt();
+                    System.out.println("Enter number2: ");
+                    int num2 = input1.nextInt();
+                    switch (op) {
+                        case 3:
+                            result = num1 * num2;
+                            System.out.printf("Result: %d * %d = %f", num1, num2, result);
+                            break;
+                        case 4:
+                            result = num1 / num2;
+                            System.out.printf("Result: %d / %d = %f", num1, num2, result);
+                            break;
+                        default:
+                            System.out.println("Enter proper input.");
+                    }
                 }
             } else nextOp = false;
         }
